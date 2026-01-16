b1c88c7be079a10434086e9f20e95a55b7f10361
KitchiDioless <dustirris@gmail.com>
2026-01-16 18:44:07 +0300

    fix: deleted some code and added appsettings copying

--- src/TaskServiceGrpc/Program.cs
+++ src/TaskServiceGrpc/Program.cs
@@ -1,4 +1,4 @@
-﻿using TaskServiceGrpc.Controllers;
+using TaskServiceGrpc.Controllers;
 using TaskServiceGrpc.Extensions;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -24,10 +24,5 @@ await app.Services.EnsureDatabaseCreatedAsync();
 app.MapGrpcService<TaskServiceController>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
-string kafkaBootstrapServers = builder.Configuration["Kafka:BootstrapServers"] ?? "localhost:9092";
-string kafkaTopic = builder.Configuration["Kafka:Topic"] ?? "TaskCreateTopic";
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-    ?? "Host=localhost;Port=5432;Database=TaskServiceDb;Username=postgres;Password=postgres";
 
 app.Run();
